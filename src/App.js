bda2871ea0a23362eb01d80aa708e83c9be57e9e
strtanberahmed <tanber1111@gmail.com>
2022-03-03 18:41:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h3>hello world</h3>
+      <h3>hello world</h3>
+      <h2>HOME PAGE</h2>
+      <h1>UI</h1>
     </div>
   );
 }
