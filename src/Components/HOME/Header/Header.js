dac22505451fb54bdd17909b4636a3402089d9a2
strtanberahmed <tanber1111@gmail.com>
2022-03-05 14:25:25 +0300

    services added

--- src/Components/HOME/Header/Header.js
+++ src/Components/HOME/Header/Header.js
@@ -15,15 +15,15 @@ const Header = () => {
     return (
         <div className='carousel container'>
             <Swiper spaceBetween={30} centeredSlides={true} autoplay={{
-                "delay": 2500,
+                "delay": 6500,
                 "disableOnInteraction": false
             }} pagination={{
                 "clickable": true
             }} navigation={true} className="mySwiper">
                 <SwiperSlide>
-                    <div className='d-flex p-4 mx-5  align-items-center'>
-                        <div className='carousel-info'>
-                            <h1>Harley Davidson T Shirt</h1>
+                    <div className='row p-4 align-items-center'>
+                        <div className='carousel-info col-md-6'>
+                            <h1 className='fw-bold fst-italic'>Harley Davidson T Shirt</h1>
                             <h6>under reasonable prices</h6>
                             <div className='price'>
                                 <p>FROM</p>
@@ -31,34 +31,41 @@ const Header = () => {
                             </div>
                             <button className='buying-btn'>Start buying</button>
                         </div>
-                        <img className='carousel-img' src={img1} />
+                        <div className="col-md-6">
+                            <img className='carousel-img' src={img1} />
+                        </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='d-flex p-4 align-items-center '>
-                        <div className='carousel-info'>
-                            <h1>Harley Davidson T Shirt</h1>
+                    <div className='row p-4 align-items-center'>
+                        <div className='carousel-info col-md-6'>
+                            <h1 className='fw-bold fst-italic'>Harley Davidson T Shirt</h1>
                             <h6>under reasonable prices</h6>
                             <div className='price'>
                                 <p>FROM</p>
-                                <h1>$65</h1>
+                                <h1>$73</h1>
                             </div>
                             <button className='buying-btn'>Start buying</button>
                         </div>
-                        <img className='carousel-img' src={img1} />
-                    </div></SwiperSlide><SwiperSlide>
-                    <div className='d-flex p-4 align-items-center'>
-                        <div className='carousel-info'>
-                            <h1>Harley Davidson T Shirt</h1>
+                        <div className="col-md-6">
+                            <img className='carousel-img' src={img1} />
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='row p-4 align-items-center'>
+                        <div className='carousel-info col-md-6'>
+                            <h1 className='fw-bold fst-italic'>Harley Davidson T Shirt</h1>
                             <h6>under reasonable prices</h6>
                             <div className='price'>
                                 <p>FROM</p>
-                                <h1>$66</h1>
+                                <h1>$73</h1>
                             </div>
                             <button className='buying-btn'>Start buying</button>
                         </div>
-                        <img className='carousel-img' src={img1} />
-
+                        <div className="col-md-6">
+                            <img className='carousel-img' src={img1} />
+                        </div>
                     </div>
                 </SwiperSlide>
             </Swiper>
--- src/Components/HOME/HomeUi/HomeUi.js
+++ src/Components/HOME/HomeUi/HomeUi.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Footer from "../../Shared/Footer/Footer";
 import Header from '../Header/Header';
 import Offer from '../Offer/Offer';
+import Services from '../Services/Services';
 
 const HomeUi = () => {
     return (
         <div>
             <Header></Header>
+            <hr className='container my-5' />
             <Offer></Offer>
+            <hr className='container my-5' />
+            <Services></Services>
             <Footer></Footer>
         </div>
     );
